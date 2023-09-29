7226f561db88eaec38c04f6e3277d2aad30d33cb
Gianluca Recchia <g.recchia@desotech.it>
2023-09-29 10:59:27 +0200

    Delete namespace during uninstall

--- pacman-uninstall.sh
+++ pacman-uninstall.sh
@@ -4,3 +4,4 @@ kubectl delete -n pacman -f security/rbac.yaml
 kubectl delete -n pacman -f security/secret.yaml
 kubectl delete -n pacman -f deployments/pacman-deployment.yaml
 kubectl delete -n pacman -f services/pacman-service.yaml
+kubectl delete namespace pacman
