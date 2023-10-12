fdb4b88b15102abf353b026c4019481ed0cb9126
Evangelista Tragni <93332785+etragni@users.noreply.github.com>
2023-10-12 13:36:38 +0200

    Update pacman-install.sh

--- pacman-install.sh
+++ pacman-install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 kubectl create namespace pacman
-kubectl create -n pacman -f security/rbac.yaml
-kubectl create -n pacman -f security/secret.yaml
-kubectl create -n pacman -f deployments/pacman-deployment.yaml
-kubectl create -n pacman -f services/pacman-service.yaml
+kubectl apply -n pacman -f security/rbac.yaml
+kubectl apply -n pacman -f security/secret.yaml
+kubectl apply -n pacman -f deployments/pacman-deployment.yaml
+kubectl apply -n pacman -f services/pacman-service.yaml
